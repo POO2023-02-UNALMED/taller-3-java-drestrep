dcf85c729e0c0a866487b9d960cc1121e5116170
drestrep <drestrepoag@unal.edu.co>
2023-08-25 01:06:03 -0500

    4th change TS

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -12,7 +12,7 @@ public class TV {
     public TV(Marca marca, boolean estado){
         this.marca = marca;
         this.estado = estado;
-        
+        numTV++;
     }
     
     public Marca getMarca(){
